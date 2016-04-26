1ce6ce586b6d7502fef24513f2a6ff09baf95750
Troy A. Sornson <troyasornson37@gmail.com>
2016-04-25 21:19:47 -0600

    Initial Commit; Adding Version 2

---
+++ README.md
@@ -0,0 +1,31 @@
+# Vici's Mod
+This is a mod I created for Fortress Craft Evolved, a game very similar to a
+heavily modded minecraft but better. I'm hoping by sharing the source code for
+my project, others may learn from me and maybe even contribute to it!
+
+I hereby give permission for anyone to use my code for their own machines.
+
+# Version 2
+PowWowMk1
+=========
+"It's like WiFi, but for power"
+
+Will wirelessly transport 10 pps per machine in a 3m radius sphere (10 ppspm).
+Cost: double of a T1 LPT - 4 Lithum, 10 Iron, 4 Tin
+
+PowWowMk2
+=========
+Will wirelessly transport 40 pps per machine in a 4m radius sphere (40 ppspm).
+Cost: double of a T2 LPT - 64 Titanium, 64 Nickel
+
+PowWowMk3
+=========
+Will wirelessly transport 320 pps per machine in a 5m radius sphere (320 ppspm).
+Cost: double of a T3 LPT - 370 Titanium, 190 Nickel, 150 Gold
+
+PowWowMk4
+=========
+Will wirelessly transport unlimited pps per machine in a 7m radius sphere.
+Cost: 1 T4 Battery - 1728 Titanium, 864 Nickel, 405 Gold, 162 Tin Plates
+
+*What you lose in capacity, you gain in powered surface area!
---
+++ Vici.VicisMod/2/src/VicisMod.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class VicisMod : FortressCraftMod {
+
+    public enum VicisModVersion {
+        Version1,
+        Version2
+    }
+
+    public const VicisModVersion CURRENT_VERSION = VicisModVersion.Version2;
+
+    private String LOGGER_PREFIX = "Vici.VicisMod";
+
+    private ushort powWowCube;
+    private ushort powWowMk1Val;
+    private ushort powWowMk2Val;
+    private ushort powWowMk3Val;
+    private ushort powWowMk4Val;
+
+    private const bool DEBUG = false;
+
+    public static void log(String prefix, String msg) {
+        if(DEBUG) {
+            UnityEngine.Debug.Log(prefix + ": " + msg);
+        }
+    }
+
+    public override ModRegistrationData Register() {
+        ModRegistrationData mrd = new ModRegistrationData();
+        mrd.RegisterEntityHandler(PowWowMk1.VALUE_NAME);
+        mrd.RegisterEntityHandler(PowWowMk2.VALUE_NAME);
+        mrd.RegisterEntityHandler(PowWowMk3.VALUE_NAME);
+        mrd.RegisterEntityHandler(PowWowMk4.VALUE_NAME);
+
+        TerrainDataEntry terrainDataEntry;
+        TerrainDataValueEntry terrainDataValueEntry;
+        TerrainData.GetCubeByKey(PowWow.CUBE_NAME, out terrainDataEntry, out terrainDataValueEntry);
+        if (terrainDataEntry != null) powWowCube = terrainDataEntry.CubeType;
+        TerrainData.GetCubeByKey(PowWowMk1.VALUE_NAME, out terrainDataEntry, out terrainDataValueEntry);
+        if (terrainDataValueEntry != null) powWowMk1Val = terrainDataValueEntry.Value;
+        TerrainData.GetCubeByKey(PowWowMk2.VALUE_NAME, out terrainDataEntry, out terrainDataValueEntry);
+        if (terrainDataValueEntry != null) powWowMk2Val = terrainDataValueEntry.Value;
+        TerrainData.GetCubeByKey(PowWowMk3.VALUE_NAME, out terrainDataEntry, out terrainDataValueEntry);
+        if (terrainDataValueEntry != null) powWowMk3Val = terrainDataValueEntry.Value;
+        TerrainData.GetCubeByKey(PowWowMk4.VALUE_NAME, out terrainDataEntry, out terrainDataValueEntry);
+        if (terrainDataValueEntry != null) powWowMk4Val = terrainDataValueEntry.Value;
+
+        log(LOGGER_PREFIX, "Registered mod");
+
+        return mrd;
+    }
+
+    public override ModCreateSegmentEntityResults CreateSegmentEntity(ModCreateSegmentEntityParameters parameters) {
+        log(LOGGER_PREFIX, "creating with params: " +
+            "X = " + parameters.X + ", " +
+            "Y = " + parameters.Y + ", " +
+            "Z = " + parameters.Z + ", " +
+            "Cube = " + parameters.Cube + ", " +
+            "Value = " + parameters.Value + ", " +
+            "Segment = " + parameters.Segment + ", " +
+            "Type = " + parameters.Type + ", " +
+            "Flags = " + parameters.Flags + ", " +
+            "toString = \"" + parameters.ToString() + "\""
+            );
+        ModCreateSegmentEntityResults res = new ModCreateSegmentEntityResults();
+
+        if (parameters.Cube == powWowCube) {
+            if (parameters.Value == powWowMk1Val) res.Entity = new PowWowMk1(parameters);
+            if (parameters.Value == powWowMk2Val) res.Entity = new PowWowMk2(parameters);
+            if (parameters.Value == powWowMk3Val) res.Entity = new PowWowMk3(parameters);
+            if (parameters.Value == powWowMk4Val) res.Entity = new PowWowMk4(parameters);
+        }
+
+        return res;
+    }
+}
